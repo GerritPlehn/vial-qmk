8deda499a3595667f6c3f61631cf6cf5910ae861
Gerrit Plehn <gerritplehn@gmail.com>
2022-07-26 14:05:12 +0200

    Q1 Vial keymap

---
+++ keyboards/keychron/q1/q1_iso_atmega32u4/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x24, 0x52, 0xF3, 0x55, 0x59, 0x1E, 0xCC, 0x6E}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
