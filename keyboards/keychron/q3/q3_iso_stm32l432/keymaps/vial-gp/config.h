bd1e41f37a229b3184992c6c44ada79f1dca9570
Gerrit Plehn <gerritplehn@gmail.com>
2022-07-27 23:36:45 +0200

    q3 user keymap

---
+++ keyboards/keychron/q3/q3_iso_stm32l432/keymaps/vial-gp/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xB5, 0x59, 0xA7, 0x81, 0xF1, 0x6B, 0x45, 0xAA}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
