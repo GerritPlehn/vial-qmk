5c384a11d60bf59cd36955dd70558280650b53c8
Gerrit Plehn <gerritplehn@gmail.com>
2022-07-29 19:17:43 +0200

    twoyo vial keymap

---
+++ keyboards/rainkeebs/twoyo/keymaps/vial/config.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x99, 0x4C, 0x5D, 0x2F, 0xCA, 0x1D, 0xB3, 0x07}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
+#define VIAL_TAP_DANCE_ENTRIES 5
+#define VIAL_COMBO_ENTRIES 5
+#define VIAL_KEY_OVERRIDE_ENTRIES 5
+
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
