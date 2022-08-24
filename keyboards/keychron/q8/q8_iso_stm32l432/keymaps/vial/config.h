50693a7073f37f05be6b652f190a18dfbcd0f0c4
Gerrit Plehn <gerritplehn@gmail.com>
2022-08-24 23:21:14 +0200

    q8 vial

---
+++ keyboards/keychron/q8/q8_iso_stm32l432/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x97, 0xDC, 0xBF, 0x9F, 0xBD, 0x57, 0x8A, 0xB8}
+
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
